84b72cddca1d4ff26cbec760cd2ff794b9e927ec
Gokul8807 <gokulvijayakumar76@gmail.com>
2025-01-29 04:15:50 +0100

    leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,15 +1,5 @@
-def is_leap_year(year):
-    if year % 400 == 0:
-        return True
-    elif year % 100 == 0:
-        return False
-    elif year % 4 == 0:
-        return True
-    return False
-
-# Example usage
-year = int(input("Type year: "))
-if is_leap_year(year):
-    print(f"{year} is a leap year.")
+year = int(input("Enter year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"Leap year: {year}")
 else:
-    print(f"{year} is not a leap year.")
+    print("Not a leap year")
